eb087c6efe6ee1017ada7586c5d2a6e8d220357f
Kazuki Tsunemi <ksilverwall@gmail.com>
2019-07-28 23:14:22 +0900

    Implement top page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Top from './Top.js'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <main>
+      <Switch>
+        <Route exact path='/' component={Top} />
+        <Route exact path='/tos' component={Tos} />
+        <Route exact path='/privacy-policy' component={PrivacyPolicy} />
+        <Route path='/' component={NotFoundPage} />
+      </Switch>
+    </main>
+    <footer className="footer">
+      <Link to="/">TOP</Link>
+      <Link to="/tos">利用規約</Link>
+      <Link to="/privacy-policy">プライバシーポリシー</Link>
+    </footer>
+  </BrowserRouter>
+)
+
+const Tos = () => (
+  <h1>Dummy Page</h1>
+)
+
+const PrivacyPolicy = () => (
+  <h1>Dummy Page</h1>
+)
+
+const NotFoundPage = () => (
+  <h1>Page Not Found</h1>
+)
 
 export default App;
