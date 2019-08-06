4ca980db2e1eed443d69d43eee7f4403402fcddc
Kazuki Tsunemi <ksilverwall@gmail.com>
2019-08-06 10:39:38 +0900

    Implement group common header tag

--- src/GroupsRouter.js
+++ src/GroupsRouter.js
@@ -3,11 +3,21 @@ import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import { NotFoundPage } from './Error.js'
 
 const GroupsRouter = () => (
-  <Switch>
-    <Route exact path='/groups/:id' component={GroupIndex} />
-    <Route exact path='/groups/:id/admin' component={GroupAdmin} />
-    <Route component={NotFoundPage} />
-  </Switch>
+  <React.Fragment>
+    <header>
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="/groups/sla">ホーム </a>
+          <a className="nav-item nav-link" href="/user/config">設定</a>
+        </div>
+      </nav>
+    </header>
+    <Switch>
+      <Route exact path='/groups/:id' component={GroupIndex} />
+      <Route exact path='/groups/:id/admin' component={GroupAdmin} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </React.Fragment>
 )
 
 class GroupIndex extends React.Component {
