eb838dfe2e5fc1d52b431d3b8f64358c773cbde7
Kazuki Tsunemi <ksilverwall@gmail.com>
2020-09-22 00:26:27 +0900

    Implement footer design

--- src/components/organisms/FooterPanel.tsx
+++ src/components/organisms/FooterPanel.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import styles from './FooterPanel.module.css';
+import { Link } from 'react-router-dom';
 
 export default () => (
-  <p>Dummy Footer</p>
+  <div className={styles.container}>
+    <div className={styles.links}>
+      <Link to="/">TOP</Link>
+      <p>|</p>
+      <Link to="/tos">利用規約</Link>
+      <p>|</p>
+      <Link to="/privacy-policy">プライバシー</Link>
+    </div>
+  </div>
 );
